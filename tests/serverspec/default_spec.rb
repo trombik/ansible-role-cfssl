ac93207210e59534ed38d99ec194ee8f81de5e42
Tomoyuki Sakurai <y@trombik.org>
2021-11-07 06:50:12 +0700

    bugfix: fix permission on .csr
    cfssl user needs it

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -92,7 +92,7 @@ describe file(csr_config) do
   it { should exist }
   it { should be_file }
   it { should be_mode 644 }
-  it { should be_owned_by "root" }
+  it { should be_owned_by user }
   it { should be_grouped_into default_group }
   its(:content_as_json) { should include("CN" => "Sensu Test CA") }
 end
